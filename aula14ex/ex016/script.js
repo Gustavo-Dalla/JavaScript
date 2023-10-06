9f52cf315a15c16f7f6a0ad6dd36d0e2180138bc
Gustavo Dallabrida <gustavodallabridaeduc@gmail.com>
2023-10-06 18:24:25 -0300

    Desenvolvimento dos exercícios
    Curso em Vídeo

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,12 +4,19 @@ function resultado(){
     var passo = Number(document.getElementById('passo').value)
     var txt = document.getElementById('txt')
     var sequencia = [] //Armazena todos os numeros em uma lista, ou então pode ser em ''
+    if(num==0){
+        txt.innerHTML = `Impossível contar!`
+    }else if(passo==0){
+        alert("Passo inválido! Começando pelo passo 1")
     do{
-        sequencia += `${num} ` //Adciona cada resultado ou seja: 10+2 -> ´10 12´ -> 12+2 -> ´10 12 14´ (Adiciona a sequencia na string).... OU ENTÃO USAR O METODO .join (mais profissional)
+        sequencia += `${num} ` //Adciona cada resultado ou seja: 10+2 -> ´10 12´ -> 12+2 -> ´10 12 14´ (Adiciona a sequencia na string).... OU ENTÃO USAR O METODO .push (mais profissional)
+        num ++
+    }while(num<=fim)
+    txt.innerHTML = sequencia //Muda o txt para a sequencia completa
+    }
+    do{
+        sequencia += `${num} ` //Adciona cada resultado ou seja: 10+2 -> ´10 12´ -> 12+2 -> ´10 12 14´ (Adiciona a sequencia na string).... OU ENTÃO USAR O METODO .push (mais profissional)
         num +=passo
     }while(num<=fim)
     txt.innerHTML = sequencia //Muda o txt para a sequencia completa
 }
-
-
-//A TERMINAR...
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,11 @@
-//A TERMINAR...
+var per = window.document.getElementById('num')
+var num = Number(per.value)
+var vezes = 0
+function resultado(){
+    while(vezes<=10){
+        var conta = num*vezes
+        document.write(`${num}*${vezes} = ${conta}`)
+        vezes++
+    }
+}
+//A terminar
