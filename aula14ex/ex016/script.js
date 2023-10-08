0efc93e5f43148c435d93291de7609c23cd2fd6b
Gustavo Dallabrida <gustavodallabridaeduc@gmail.com>
2023-10-07 23:17:15 -0300

    Resolução de Exercícios
    Curso em Vídeo

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,4 +1,38 @@
-function resultado(){
+//RESOLUÇÃO
+
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){ //Se o input ficar vazio: alert
+        window.alert("[ERRO] faltam dados!")
+        res.innerHTML = `Impossível contar!`
+    }else{
+        res.innerHTML = `Contando: <br>` //Se o input não fica vazio faça a função das contas
+        let i = Number(ini.value) //Puxando a variavel no tipo número e o valor dela
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){ //Se passo for 0 ele vai fazer passo 1 ou seja sempre +1
+            window.alert(`Passo invalido! Considerando PASSO 1`)
+            p = 1 //Passo vai ser um e vai para baixo
+        }
+        if(i < f){ //Se o inicio(20) for menor que o fim(21) faz a função de SOMA de passo 
+            for(let c = i; c <= f; c += p){ //Loop c(começo) é o inicio o valor que a pessoa informar exeplo 21, enquanto o 21 for menor que 25 ele faz e soma 1 (exemplo) e faz isso até ser maior ou igual
+                res.innerHTML += `${c} \u{1F449}` //Dentro da variavel res adiciona a variavel c já somada mais um emoji e faz o loop novamente até o c for maior que o fim
+        }
+    }else{ //Senão o inicio(20) for maior que o fim (15) faz a função de SUBTRAÇÃO de passo
+        for(let c = i; c >= f; c -=p){ //Loop c(começo) é o valor inicial, exemplo 25, enquanto 25 for maior que 20 ele faz o loop e tira um(passo): 25 -> 24 -> 23.... -> 20 -> Para
+                res.innerHTML += `${c} \u{1F449}`//Dentro da variavek res ele adiciona(concatena) o valor inicial após a operação da conta e um emoji até atingir a requisição
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
+
+
+/*function resultado(){
     var num = Number(document.getElementById('num').value)
     var fim = Number(document.querySelector('input#fim').value)
     var passo = Number(document.getElementById('passo').value)
@@ -19,4 +53,4 @@ function resultado(){
         num +=passo
     }while(num<=fim)
     txt.innerHTML = sequencia //Muda o txt para a sequencia completa
-}
+}*/
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,11 +1,22 @@
-var per = window.document.getElementById('num')
-var num = Number(per.value)
-var vezes = 0
 function resultado(){
+    var num = Number(window.document.getElementById('num').value)
+    var vezes = 0
     while(vezes<=10){
         var conta = num*vezes
         document.write(`${num}*${vezes} = ${conta}`)
         vezes++
     }
 }
-//A terminar
+    //O código apenas funciona com o var num dentro da função. Explicação:
+    
+    /*Em JavaScript, as variáveis têm escopos, que determinam onde uma variável pode ser acessada e usada. Existem dois principais tipos de escopo em JavaScript:
+
+Escopo Global: Variáveis declaradas fora de qualquer função têm escopo global. Isso significa que essas variáveis podem ser acessadas e modificadas de qualquer lugar no seu código JavaScript, incluindo funções.
+
+Escopo Local (ou de Função): Variáveis declaradas dentro de uma função têm escopo local. Isso significa que elas só podem ser acessadas e usadas dentro dessa função específica.
+
+No seu código, a variável num está declarada fora da função resultado, o que a coloca no escopo global. Quando o código JavaScript é carregado em uma página da web, ele é executado pelo navegador em um ambiente global. No entanto, ele só é executado uma vez, quando a página é carregada.
+
+Se você declarar a variável num fora da função resultado, ela será inicializada com o valor do elemento de ID 'num' no momento em que o código é carregado. Se o elemento 'num' ainda não existe quando o código é executado, document.getElementById('num') retornará null e, portanto, num será definido como NaN (Not-a-Number).*/ 
+
+//É possível declarar a função global porém há de chamar na função exemplo: var num; function ...(){num = window.document.getElementById('num')}
