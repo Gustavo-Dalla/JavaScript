b07bb45de0ac08ea0bf4d84dbd55fe26f24b59fb
Gustavo Dallabrida <gustavodallabridaeduc@gmail.com>
2023-10-30 19:58:17 -0300

    Exercicío Correção
    Curso em Vídeo

--- aula016ex/ex018(correcao)/script.js
+++ aula016ex/ex018(correcao)/script.js
@@ -1,28 +1,66 @@
-let num = document.querySelector('input#fnum'.value)
-let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#res')
-let valores = []
+let num;
+let lista;
+let res;
+let valores = [];
 
-function isNumero(n){
-    if(Number(n) >=1 && Number(n) <=100){
-        return true
-    }else{
-        return false
-    }
-}
-function inLista(n, l){
-    if (l.indexOf(Number(n)) != -1){
-        return true
-    }else{
-        return false
+
+document.addEventListener("DOMContentLoaded", function() {
+    num = document.querySelector('input#fnum');
+    lista = document.querySelector('select#flista');
+    res = document.querySelector('div#res');
+}); /*identificados por num, lista e res são buscados com document.querySelector somente quando o documento estiver pronto, evitando assim erros.*/
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-function adicionar(){
-    if(isNumero(num) && !inLista(num, valores)){
-        window.alert('Tudo ok')
-    }else{
-        window.alert('Valor inválido ou já encontrado na lista.')
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    } else {
+        return false;
     }
 }
-//A TERMINAR...
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.');
+    }
+    num.value = ''
+    num.focus() //Coloca o foco de volta dentro do input (a barrinha)
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert(`Adicione valores antes de finalizar`)
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores [0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
+}
